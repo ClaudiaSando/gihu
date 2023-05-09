53a3476c47da15dc046ea6c91c599b9fd0a0fe0d
Sergio Enrique Galván Barrios <64383615+galvanbarrios@users.noreply.github.com>
2023-05-08 22:32:37 -0600

    Cambios en el homepage
    Uso de .ts en el main, estilos, y agregar fotos y footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -5,6 +5,18 @@ import { Component } from '@angular/core';
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
+
 export class MainComponent {
-  
+
+  posters: any = [
+    {img: "../../assets/img/guardianes.jpg", titulo: "Guardianes Vol. 3", pagar: "Comprar"},
+    {img: "../../assets/img/mario2.webp", titulo: "Super Mario Bros", pagar: "Comprar"},
+    {img: "../../assets/img/john.jpg", titulo: "John Wick 3 Remaster", pagar: "Comprar"},
+    {img: "../../assets/img/oppen.webp", titulo: "Oppenheimer", pagar: "Reservar"},
+    {img: "../../assets/img/barbie.jpg", titulo: "Barbie", pagar: "Reservar"},
+    {img: "../../assets/img/exorcista.jpg", titulo: "Exorcista del Papa", pagar: "Comprar"},
+    {img: "../../assets/img/evil.jpg", titulo: "Evil Dead Rise", pagar: "Comprar"},
+    {img: "../../assets/img/flash.webp", titulo: "The Flash", pagar: "Reservar"}
+  ];
+
 }
