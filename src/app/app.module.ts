3736de945c3d1b27eb908a4515c12753eb86af31
Sergio Enrique Galván Barrios <64383615+galvanbarrios@users.noreply.github.com>
2023-05-09 17:31:46 -0600

    Cambios de estética
    Agregué un modal para cada una de esas películas y agregar la parte del calendario, solo que lo subo de una vez ya que necesito instalar algo porque la última vez me dio problemas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
@@ -18,7 +18,10 @@ import { FooterComponent } from './footer/footer.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-
+    CommonModule
+  ],
+  exports: [
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
